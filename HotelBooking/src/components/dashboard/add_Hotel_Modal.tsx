87985dea4bc673eb31540619e63b02e4c9ab4fdf
Cory <note10litenote10lite@gmail.com>
2025-01-12 02:08:28 +0800

    FIX: 巢狀<form>新增房型, 初步邏輯完畢

--- HotelBooking/src/components/dashboard/add_Hotel_Modal.tsx
+++ HotelBooking/src/components/dashboard/add_Hotel_Modal.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { add_Hotel_Detail_Interface, add_Review_Type_Interface } from "@/types/add_Hotel_Detail";
+import { useState, useEffect } from "react";
+import { add_Hotel_Detail_Interface, add_Review_Type_Interface, add_Hotel_Room_Type_Interface } from "@/types/add_Hotel_Detail";
 import { v4 as uuidv4 } from 'uuid'; 
 
 // 1. 設施初始值
@@ -23,7 +23,59 @@ export default function Add_Hotel_Modal() {
   const [date, set_Date] = useState((new Date()).toLocaleDateString());
   // console.log(date, "今天");
 
-  const submit = (formData: FormData) => {
+  // 3. 指定飯店 - 所有房型列表
+  const [roomTypes, set_RoomTypes] = useState<add_Hotel_Room_Type_Interface[]>([
+    // {
+    //   room_Type: "",
+    //   roomType_Id: uuidv4(),
+    //   room_Price: null,
+    //   room_Availability: null,
+    //   smoke: null,
+    //   amenity_List: [],
+    //   room_Size: null,
+    //   max_People: null,
+    //   roomType_Image_List: [],
+    // }
+  ])
+
+  // 4. 再新增一個房型
+  const add_Room_Type = () => {
+    set_RoomTypes([...roomTypes, {
+      room_Type: "",
+      roomType_Id: uuidv4(),
+      room_Price: null,
+      room_Availability: null,
+      smoke: null,
+      amenity_List: [],
+      room_Size: null,
+      max_People: null,
+      roomType_Image_List: []
+    }])
+  };
+
+  // 5. 組件載入時，預先加一個房型
+  useEffect(() => {
+    add_Room_Type()
+  },[])
+
+  // 5. 移除房型
+  const remove_Room_Type = (index: number) => {
+    set_RoomTypes(roomTypes.filter((_, i) => i !== index))
+  };
+
+  // 6. 處理房型表單變更
+  const handle_Room_Type_Change = (index: number, field: keyof add_Hotel_Room_Type_Interface, value: any) => {
+    const updateRoomTypes = roomTypes.map((room, i) => 
+      i === index ? {...room, [field]: value} : room
+    );
+    set_RoomTypes(updateRoomTypes)
+  }
+  
+  // 7. 提交 <form> 函式
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event?.currentTarget);
+
     // 3. <form> 拿數據
     const hotel_Id = (hotel_List.length +1).toString(); // 自定義id, 從hotel_List長度去拉
     const hotel_Name = formData.get("hotelname") as string | null;
@@ -46,13 +98,18 @@ export default function Add_Hotel_Modal() {
     const date = formData.get("date") as string | null;
     const traveler_Rating = formData.get("rating") as string | null;
     const comment = formData.get("comment") as string | null;
-    const roomtype = formData.get("roomtype");
-    const roomprice = formData.get("roomprice")
-    const availability = formData.get("availability");
-    const smoke = formData.get("smoke");
-    const amenity = formData.getAll("amenity");
-    const roomsize = formData.get("roomsize");
-    const maxpeople = formData.get("maxpeople");
+    const room_Type = formData.get("roomtype") as string | null;;
+    const room_Price = formData.get("roomprice") as number | null;
+    const room_Availability = formData.get("availability") as number | null;;
+    const smoke = formData.get("smoke") as string | null; // 打勾是"on" 或者 沒勾是 null
+    const amenity_List = formData.getAll("amenity") as string[] | null;;
+    const room_Size = formData.get("roomsize") as number | null;;
+    const max_People = formData.get("maxpeople") as number | null;;
+    const roomTypeFiles = formData.getAll("roomtypeimages") as File[];
+    const roomType_Image_List = roomTypeFiles.map((item) => ({
+      url: URL.createObjectURL(item),
+      description: "",
+    }))
 
     
   // 4. 添加評論函數 (這函式要UI去按按鈕才會觸發)
@@ -86,7 +143,26 @@ export default function Add_Hotel_Modal() {
       });
     };
 
-    // 6. 構建新的酒店物件
+    // 6. 眾多房型Array & 單一房型 - 初始值
+    // let initialRoomType_List: add_Hotel_Room_Type_Interface[] = [];
+    // if(room_Type && room_Price && room_Availability && smoke && amenity_List && room_Size && max_People && roomType_Image_List){
+    //   initialRoomType_List.push({
+    //     room_Type: room_Type,
+    //     roomType_Id: uuidv4(),
+    //     room_Price: room_Price,
+    //     room_Availability: room_Availability,
+    //     smoke: smoke,
+    //     amenity_List: amenity_List,
+    //     room_Size: room_Size,
+    //     max_People: max_People,
+    //     roomType_Image_List: roomType_Image_List
+    //   })
+    // }
+
+    // 6. 眾多房型Array & 單一房型 - 初始值
+    const initialRoomType_List = roomTypes
+
+    // 7. 構建新的酒店物件
     const new_Hotel: add_Hotel_Detail_Interface = {
       hotel_Id: hotel_Id,
       hotel_Name: hotel_Name,
@@ -102,6 +178,7 @@ export default function Add_Hotel_Modal() {
       hotel_Intro: intro,
       hotel_Image_List: hotel_Image_List,
       review_List: initialReviews_List,
+      roomType_List: initialRoomType_List,
     };
 
 
@@ -114,7 +191,7 @@ export default function Add_Hotel_Modal() {
   return <div className="flex flex-col p-4 gap-4 bg-lightGray">
     <p className="text-lg font-bold">Add New Hotel</p>
 
-    <form action={submit} className="flex flex-col gap-4">
+    <form onSubmit={submit} className="flex flex-col gap-4">
 
       <div className="flex flex-col bg-white rounded border border-softGray p-2 gap-2">
         <p className="font-semibold text-primary">Basic Information</p>
@@ -247,10 +324,17 @@ export default function Add_Hotel_Modal() {
       {/** 創建房型 */}
       <div className="flex flex-col bg-white rounded border border-softGray p-2 gap-2">
         <p className="font-semibold text-primary">Room Type</p>
-        
+
+        {roomTypes.map((roomType, index) => {
+          return <div key={roomType.roomType_Id} className="flex flex-col bg-gray-100 rounded border border-gray-300 p-2 gap-2 mb-2">
+            <h4 className="font-semibold">Room Type {index + 1}</h4>
+
+
+
         {/** 選擇房型 */}
-        <label htmlFor="roomtype" className="font-semibold">Select Room Type</label>
-        <select name="roomtype" id="roomtype" className="border rounded">
+        <label htmlFor={`roomtype_${roomType.roomType_Id}`} className="font-semibold">Select Room Type</label>
+        <select name={`roomtype[${index}].room_Type`} id={`roomtype_${roomType.roomType_Id}`} className="border rounded" 
+        value={roomType.room_Type} onChange={(event) => handle_Room_Type_Change(index, "room_Type", event.target.value)} required>
           {roomType_List.map((room) => {
             return <option key={room} value={room}>
               {room.charAt(0).toUpperCase() + room.slice(1)}
@@ -262,16 +346,24 @@ export default function Add_Hotel_Modal() {
         {/** 房型價錢、 該房型共幾間、吸菸房*/}
         <div className="flex gap-2">
           <div className="flex gap-2">
-            <label htmlFor="roomprice">Price</label>
-            <input type="number" id="roomprice" name="roomprice" className="border rounded no-spin max-w-20"/>
+            <label htmlFor={`roomprice_${roomType.roomType_Id}`}>Price</label>
+            <input type="number" id={`roomprice_${roomType.roomType_Id}`} name={`roomTypes[${index}].room_Price`} className="border rounded no-spin max-w-20"
+            value={roomType.room_Price ?? ""} 
+            onChange={(event) => handle_Room_Type_Change(index, "room_Price", event.target.value ? Number(event.target.value) : null)}
+            required/>
           </div>
           <div className="flex gap-2">
-            <label htmlFor="availability">Availability 該房型共幾間</label>
-            <input type="number" id="availability" name="availability" className="border rounded no-spin max-w-20"/>
+            <label htmlFor={`availability_${roomType.roomType_Id}`}>Availability 該房型共幾間</label>
+            <input type="number" id={`availability_${roomType.roomType_Id}`} name={`roomTypes[${index}].room_Availability`} className="border rounded no-spin max-w-20"
+            value={roomType.room_Availability ?? ""}
+            onChange={(event) => handle_Room_Type_Change(index, "room_Availability", event.target.value ? Number(event.target.value): null)}
+            required/>
           </div>
           <div className="flex gap-2">
-            <input type="checkbox" id="smoke" name="smoke" value={"smokingroom"}/>
-            <label htmlFor="smoke">Smoking Room</label>
+            <input type="checkbox" id={`smoke_${roomType.roomType_Id}`} name={`roomTypes[${index}].smoke`}
+            checked={roomType.smoke === "smokingroom"} value={"smokingroom"}
+            onChange={(event) => handle_Room_Type_Change(index, "smoke", event.target.checked ? "smokingroom" : "non-smoking")}/>
+            <label htmlFor={`smoke_${roomType.roomType_Id}`}>Smoking Room</label>
           </div>
         </div>
         {/** 房型價錢、 該房型共幾間、吸菸房 */}
@@ -283,8 +375,28 @@ export default function Add_Hotel_Modal() {
             <div className="flex gap-2">
               {amenity.map((amenity) => {
                 return <div className="flex gap-2" key={amenity}>
-                  <input type="checkbox" id={amenity} name="amenity" value={amenity}/>
-                  <label htmlFor={amenity}>{amenity.charAt(0).toUpperCase() + amenity.slice(1)}</label>
+                  <input type="checkbox" id={`${amenity}_${roomType.roomType_Id}`} name={`roomTypes[${index}].amenity_List`} 
+                  value={amenity} 
+                  checked={roomType.amenity_List?.includes(amenity) ?? false}
+                  onChange={(e) => {
+                    const isChecked = e.target.checked;
+                    let updatedAmenities = roomType.amenity_List
+                      ? [...roomType.amenity_List]
+                      : [];
+                    if (isChecked) {
+                      updatedAmenities.push(amenity);
+                    } else {
+                      updatedAmenities = updatedAmenities.filter(
+                        (a) => a !== amenity
+                      );
+                    }
+                    handle_Room_Type_Change(
+                      index,
+                      "amenity_List",
+                      updatedAmenities
+                    );
+                  }}/>
+                  <label htmlFor={`${amenity}_${roomType.roomType_Id}`}>{amenity.charAt(0).toUpperCase() + amenity.slice(1)}</label>
                 </div>
               })}
             </div>
@@ -295,16 +407,57 @@ export default function Add_Hotel_Modal() {
         {/** 房型大小、最多住幾人 */}
         <div className="flex gap-2">
           <div className="flex gap-2">
-            <label htmlFor="roomsize">Room Size {` (㎡)`} </label>
-            <input type="number" id="roomsize" name="roomsize" className="border rounded no-spin max-w-20"/>
+            <label htmlFor={`roomsize_${roomType.roomType_Id}`}>Room Size {` (㎡)`} </label>
+            <input type="number" id={`roomsize_${roomType.roomType_Id}`} name={`roomTypes[${index}].room_Size`} className="border rounded no-spin max-w-20"
+            value={roomType.room_Size ?? ""}
+            onChange={(event) => handle_Room_Type_Change(index, "room_Size", event.target.value ? Number(event.target.value): null)}
+            required/>
           </div>
 
           <div className="flex gap-2">
-            <label htmlFor="maxpeople">Max People</label>
-            <input type="number" id="maxpeople" name="maxpeople" className="border rounded no-spin max-w-20"/>
+            <label htmlFor={`maxpeople_${roomType.roomType_Id}`}>Max People</label>
+            <input type="number"  id={`maxpeople_${roomType.roomType_Id}`}  name={`roomTypes[${index}].max_People`} className="border rounded no-spin max-w-20"
+            value={roomType.max_People ?? ""}
+            onChange={(event) => handle_Room_Type_Change(index, "max_People", event.target.value ? Number(event.target.value) : null)}
+            required/>
           </div>
         </div>
         {/** 房型大小、最多住幾人 */}
+
+        {/** 上傳房型輪播圖 */}
+        <div className="flex flex-col gap-2">
+          <label htmlFor={`roomtypeimages_${roomType.roomType_Id}`} className="font-semibold text-primary">Upload Images</label>
+          <input type="file"  id={`roomtypeimages_${roomType.roomType_Id}`}  name={`roomTypes[${index}].room_Image_List`} accept="images/*"
+          multiple className="border rounded" 
+          onChange={(e) => {
+            if (e.target.files) {
+              const files = Array.from(e.target.files);
+              const imageList = files.map((file) => ({
+                url: URL.createObjectURL(file),
+                description: "",
+              }));
+              handle_Room_Type_Change(
+                index,
+                "roomType_Image_List",
+                imageList
+              );
+            }
+          }}/>
+        </div>
+        {/** 上傳房型輪播圖 */}
+
+        
+           {/* 添加房型按鈕 */}
+          <button type="button" onClick={add_Room_Type} className="bg-primary text-white px-4 py-2 rounded">
+            再新增一個房型
+          </button>
+
+
+
+          </div>
+        })}
+        
+
 
 
 
--- HotelBooking/src/types/add_Hotel_Detail.ts
+++ HotelBooking/src/types/add_Hotel_Detail.ts
@@ -15,7 +15,7 @@ export interface add_Hotel_Detail_Interface {
   checkin: string | null,
   checkout: string | null,
   review_List: add_Review_Type_Interface[],
-  // roomType: add_Hotel_Room_Type_Interface[]
+  roomType_List: add_Hotel_Room_Type_Interface[]
 }[];
 
 // 2. 旅客評論
@@ -30,13 +30,14 @@ export interface add_Review_Type_Interface {
 // 2. 房型 (主流做法：同一層就能看到 roomType、images)
 export interface add_Hotel_Room_Type_Interface {
   // 2.1 以 roomType 區分單人房 / 雙人房 / 其他房型...
-  roomType: "singleRoom" | "doubleRoom"; // 2.2 有額外房型, 如familyRoom, 再透過聯合類型 | "familyRoom" 自行補上
-  id: string,
-  price: number | null,
-  images: {url: string, description: string}[],
-  availability: number,
-  smoke: boolean | null,
-  amenity: string[],
-  roomsize: number | null,
-  maxOccupancy: number | null;
+  room_Type: "singleRoom" | "doubleRoom" | "twinRoom" | "queenRoom" | "kingRoom" | string; // 2.2 有額外房型, 如familyRoom, 再透過聯合類型 | "familyRoom" 自行補上
+  // room_Type: string | null,
+  roomType_Id: string,
+  room_Price: number | null,
+  roomType_Image_List: {url: string, description: string}[],
+  room_Availability: number | null,
+  smoke: string | null,
+  amenity_List: string[] | null,
+  room_Size: number | null,
+  max_People: number | null;
 }
