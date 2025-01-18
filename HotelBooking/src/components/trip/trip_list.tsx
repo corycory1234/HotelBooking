13ed13174c8f650162a90ab55e3de36249353d4e
Cory <note10litenote10lite@gmail.com>
2025-01-19 01:45:30 +0800

    STYLE: 訂單列表完成

--- HotelBooking/src/components/trip/trip_list.tsx
+++ HotelBooking/src/components/trip/trip_list.tsx
@@ -1,18 +1,56 @@
 'use client';
 import Previous_Page from "../previous_Page/previous_Page";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Booking_List_Json from "@/fakeData/trip_List.json";
 
 const booking_Buttons = ["Upcoming", "Completed", "Cancelled"];
-
+interface Booking_List_Interface {
+  booking_Id: string,
+  booking_Img: string,
+  hotel_Name: string,
+  room_Type: string,
+  booking_Status: string,
+  checkin: string,
+  checkout: string,
+  price: number
+}
 
 export default function Trip_List () {
   // 1. 當前頁面 - 頂端文字，props傳遞給 <Previous_Page>
   const current_Page_Name = "My Bookings"
 
   // 2. Tab 切換訂單按鈕 - 高亮
-  const [tab, set_Tab] = useState(0);
+  const [tab, set_Tab] = useState("Upcoming");
 
-  return <div className="flex flex-col">
+  // 3.
+  const [booking_List, set_Upcoming_Booking_List] = useState<Booking_List_Interface[]>([]);
+  const select_Booking_Status = (tab_Text: string) => {
+    if(tab_Text === "Upcoming") {
+      const pending_Booking = Booking_List_Json.filter((item) => {
+        return item.booking_Status === "pending"
+      })
+      set_Upcoming_Booking_List(pending_Booking);
+      set_Tab(tab_Text)
+    } else if (tab_Text === "Completed") {
+      const complted_Booking = Booking_List_Json.filter((item) => {
+        return item.booking_Status === "completed";
+      });
+      set_Upcoming_Booking_List(complted_Booking);
+      set_Tab(tab_Text)
+    } else if (tab_Text === "Cancelled") {
+      const cancelled_Booking = Booking_List_Json.filter((item) => {
+        return item.booking_Status === "cancelled";
+      });
+      set_Upcoming_Booking_List(cancelled_Booking);
+      set_Tab(tab_Text);
+    }
+  }
+
+  useEffect(() => {
+    select_Booking_Status("Upcoming")
+  },[])
+
+  return <div className="flex flex-col pb-20">
 
   {/** 返回上一頁 */}
   <Previous_Page current_Page_Name={current_Page_Name}></Previous_Page>
@@ -23,59 +61,66 @@ export default function Trip_List () {
     {booking_Buttons.map((item, index) => {
       return <button key={index}
         className={` text-softGray p-2 border border-gray rounded
-        ${tab === index ? 'bg-primary text-white' : ''}`}
-        onClick={() => set_Tab(index)}> {item}
+        ${tab === item ? 'bg-primary text-white' : ''}`}
+        onClick={() => select_Booking_Status(item)}> {item}
       </button>
     })}
   </div>
   {/** Tab 切換訂單按鈕 */}
 
   {/** 訂單卡片 */}
-  <div className="flex flex-col p-4">
-    
-    <div className="flex flex-col gap-4 bg-white rounded-lg px-2 py-4">
-      {/** 訂單照片、飯店名、房型名、確認|未確認|取消 */}
-      <div className="flex gap-2">
-        <img src="/triplist/trip_1.jpg" alt="" className="w-1/3 rounded"/>
-        <div className="flex flex-col basis-1/2">
-          <p className="font-semibold">The Cargo Hotel</p>
-          <p className="text-sm">Single Room</p>
-          <span className="bg-green-200 text-green-700 rounded p-2 w-fit">Confirmed</span>
-        </div>
-      </div>
-      {/** 訂單照片、飯店名、房型名、確認|未確認|取消 */}
+  <div className="flex flex-col p-4 gap-4">
+    {
+      booking_List.map((item) => {
+        return <div className="flex flex-col gap-4 bg-white rounded-lg px-2 py-4" key={item.booking_Id}>
+            {/** 訂單照片、飯店名、房型名、確認|未確認|取消 */}
+            <div className="flex gap-2">
+              <img src={item.booking_Img} alt="" className="w-1/3 rounded"/>
+              <div className="flex flex-col basis-1/2">
+                <p className="font-semibold">{item.hotel_Name}</p>
+                <p className="text-sm">{item.room_Type}</p>
+                <span className={` rounded p-2 w-fit 
+                  ${item.booking_Status === "cancelled" ? 'bg-red-300 text-red-700'  : 'bg-green-200 text-green-700'}`}
+                >{item.booking_Status}</span>
+              </div>
+            </div>
+            {/** 訂單照片、飯店名、房型名、確認|未確認|取消 */}
 
-      <div className="border-b border-softGray"></div>
+            <div className="border-b border-softGray"></div>
 
-      {/** 入住、退房時間 */}
-      <div className="flex">
-        <div className="w-1/2 flex flex-col">
-          <p className="text-gray">Check-In</p>
-          <p className="font-semibold">Dec 15, 2024</p>
-        </div>
-        <div className="w-1/2 flex flex-col">
-          <p className="text-gray">Check-Out</p>
-          <p className="font-semibold">Dec 18, 2024</p>
-        </div>
-      </div>
-      {/** 入住、退房時間 */}
+            {/** 入住、退房時間 */}
+            <div className="flex">
+              <div className="w-1/2 flex flex-col">
+                <p className="text-gray">Check-In</p>
+                <p className="font-semibold">{item.checkin}</p>
+              </div>
+              <div className="w-1/2 flex flex-col">
+                <p className="text-gray">Check-Out</p>
+                <p className="font-semibold">{item.checkout}</p>
+              </div>
+            </div>
+            {/** 入住、退房時間 */}
 
-      <div className="border-b border-softGray"></div>
-
-
-      {/** 訂單號碼、價錢、查看訂單 */}
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-between">
-          <p className="text-gray">Booking ID: #123456</p>
-          <p className="font-semibold">$1,250</p>
-        </div>
-        <button className="bg-primary text-white rounded py-2">View Details</button>
-      </div>
-      {/** 訂單號碼、價錢、查看訂單 */}
+            <div className="border-b border-softGray"></div>
 
 
+            {/** 訂單號碼、價錢、查看訂單 */}
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-between">
+                <p className="text-gray">Booking ID: #{item.booking_Id}</p>
+                <p className="font-semibold">${item.price}</p>
+              </div>
+              <button className="bg-primary text-white rounded py-2">View Details</button>
+            </div>
+            {/** 訂單號碼、價錢、查看訂單 */}
 
-    </div>
+
+
+          </div>        
+
+
+      })
+    }
 
   </div>
   {/** 訂單卡片 */}
