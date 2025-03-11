37f9182dc33ad2e8add3888a6dd12dd33cca090d
Cory <note10litenote10lite@gmail.com>
2025-03-11 19:16:46 +0800

    fix: i18n for Booking Status in English; style: fix the height for TripList

--- HotelBooking/src/app/[locale]/trip/page.tsx
+++ HotelBooking/src/app/[locale]/trip/page.tsx
@@ -1,6 +1,6 @@
 import Trip_List from "@/components/trip/trip_list"
 export default function Trip () {
-  return <>
+  return <div className="min-h-[85vh]">
   <Trip_List></Trip_List>
-  </>
+  </div>
 }
